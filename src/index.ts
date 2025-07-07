028a2559d065a13ab40f20aaa8a57d22bf2381a3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-07-07 20:02:02 -0300

    chore: run lint script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get('/', () => {
+  return { message: 'Hello Elysia' }
+})
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.listen({ port: 3000 }, (server) => {
+  console.log(`🦊 Elysia is running at ${server.url.toString()}`)
+})
