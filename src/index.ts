92557738d0cb215c7466ea0ac0ddeb8e206b2a66
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-07-08 07:54:04 -0300

    feat: add swagger config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import { Elysia } from 'elysia'
+import swagger from '@elysiajs/swagger'
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get('/', () => {
-  return { message: 'Hello Elysia' }
-})
+const routes = new Elysia().get(
+  '/',
+  () => {
+    return { message: 'Hello Elysia' }
+  },
+  { response: { 200: t.Object({ message: t.String() }) } }
+)
+
+const app = new Elysia()
+  .use(
+    swagger({
+      documentation: {
+        servers: [
+          { url: `http://localhost:${Bun.env.PORT}` },
+          { url: 'https://books-api-elysia.onrender.com' },
+        ],
+      },
+      path: '/docs',
+    })
+  )
+  .group('/v1', (app) => app.use(routes))
 
 app.listen({ port: Bun.env.PORT }, (server) => {
   console.log(`🦊 Elysia is running at ${server.url.toString()}`)
